c15ecd9b5879b34f51043f83b4ffbdf0c9c58e8a
karljgriffin <76873290+karljgriffin@users.noreply.github.com>
2021-11-18 10:49:23 +0000

    Adding code for practical06

---
+++ practical06/README.md
@@ -0,0 +1,4 @@
+This contains the code for practical06:
+1. matmult contains the whole matrix multiplication in one script
+2. The Step6 folder contains a main script and a mm script, the latter which contains the matrix multiplication function and is called from the main script
+3. Makefile is then used to simplify / organise the code for compilation. This is particularly useful if working with massive blocks of code with many different scripts
---
+++ practical06/matmult.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() {
+
+	int n=5, p=3, q=4;
+	//Arrays below are static arrays, i.e. size is fixed
+	double A[n][p], B[p][q], C[n][q];
+	//Below are the loop indices
+	int i, j, k; 
+	
+	//Initialise A, B, C matrices
+	//2-D arrays so we will need two loops to initialise them, i.e. nested loops
+	for(i=0; i<n; i++) {
+		for (j=0; j<p; j++) { 
+			A[i][j]=i+j;
+		}
+	}
+	for(i=0; i<p; i++) {
+		for (j=0; j<q; j++) { 
+			B[i][j]=i-j;
+		}
+	}
+	for(i=0; i<n; i++) {
+		for (j=0; j<q; j++) { 
+			C[i][j]=0.0;
+		}
+	}
+	//Perform matrix multiplication
+	for(i=0; i<n; i++)
+		for(j=0; j<q; j++)
+			for(k=0; k<p; k++)
+				C[i][j]=C[i][j]+A[i][k]*B[k][j];
+				
+	//Print out the matrices
+	printf("\nThis is matrix A:\n\n");
+	for(i=0; i<n; i++) {
+		for(j=0; j<p; j++) {
+			printf("%3.0f", A[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nThis is matrix B:\n\n");
+	for(i=0; i<p; i++) {
+		for(j=0; j<q; j++) {
+			printf("%3.0f", B[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nThis is matrix C:\n\n");
+	for(i=0; i<n; i++) {
+		for(j=0; j<q; j++) {
+			printf("%3.0f", C[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
